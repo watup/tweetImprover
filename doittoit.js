dcb6214c87cd51735581f0d7e15341a240d6d0e4
Gareth Redman <gredman@nil.gen.nz>
2014-12-17 18:17:42 +1300

    General enhancements.

--- doittoit.js
+++ doittoit.js
@@ -1,15 +1,1 @@
-  
-  var tweetText = $(".tweet-text");
-
-  tweetText.each(function() {
-	var newText = "";
-	var currentText = $(this).text();
-	if(currentText.length < 4) {
-		newText = "8=D";
-	} else {
-		newText = "8" + Array(currentText.length-1).join("=") + "D";
-	}
-    $(this).text(newText);
-  });
-  
-  $('.js-media-container').html('');
+$(".tweet[data-screen-name='tubbycontroller']").remove();
